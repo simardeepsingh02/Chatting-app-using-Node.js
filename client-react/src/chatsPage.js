f5d5b6f6a39df5d13c3e87322faee46896fca6ad
simar30122000 <iamsimar12@gmail.com>
2024-03-03 19:14:42 +0530

    new

--- client-react/src/chatsPage.js
+++ client-react/src/chatsPage.js
@@ -1,14 +1,15 @@
-import { PrettyChatWindow } from "react-chat-engine-pretty";
+import { MultiChatSocket,MultiChatWindow,useMultiChatLogic } from "react-chat-engine-advanced";
 
 const ChatsPage = (props) => {
+  const chatProps=useMultiChatLogic(
+    process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID,
+    props.user.username,
+    props.user.secret
+  )
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <PrettyChatWindow
-        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
-        username={props.user.username} // adam
-        secret={props.user.secret} // pass1234
-        style={{ height: "100%" }}
-      />
+    <div style={{height:'100vh'}}>
+      <MultiChatSocket {...chatProps}/>
+      <MultiChatWindow {...chatProps} style={{height:'100%'}}/>
     </div>
   );
 };
